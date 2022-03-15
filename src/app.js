bd0f40de53f64486acb5d39084822a4ae09e7f21
shisan <820373584@qq.com>
2022-03-15 19:30:09 +0800

    判断s-button-group中的slot是否都为s-button

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Button from "/src/button";
 import Icon from "./icon";
+import ButtonGroup from "./buttonGroup";
+Vue.component('s-button-group',ButtonGroup)
 Vue.component('s-button',Button)
 Vue.component('s-icon',Icon)
 new Vue({
---
+++ src/buttonGroup.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="s-button-group">
+    <slot>
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "buttonGroup",
+  mounted() {
+    for(let node of this.$el.children){
+      if(node.nodeName.toLowerCase() !=='button' && !node.className.includes('s-button') ){
+        console.warn(`s-button-group 的子元素应该全是s-button,但是你写的是 ${node.nodeName}`)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" >
+.s-button-group{
+  display: inline-flex;
+  vertical-align: middle;
+
+  >.s-button{
+    border-radius: 0;
+    &:not(:first-child){
+      margin-left: -1px;
+    }
+    &:first-child{
+      border-top-left-radius:var(--border-radius);
+      border-bottom-left-radius:var(--border-radius);
+    }
+    &:last-child{
+      border-top-right-radius:var(--border-radius);
+      border-bottom-right-radius:var(--border-radius);
+    }
+    &:hover{
+      position: relative;
+      z-index: 1;
+    }
+
+  }
+}
+</style>
