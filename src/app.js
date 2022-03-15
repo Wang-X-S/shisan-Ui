4557df8e857260855e06678dfe4c5af971f1f832
shisan <820373584@qq.com>
2022-03-15 20:40:37 +0800

    使用chai添加四个测试用例

--- src/app.js
+++ src/app.js
@@ -11,3 +11,79 @@ new Vue({
         loading1:false,
     }
 })
+//单元测试
+import chai from 'chai'
+const expect = chai.expect
+
+//判断button的属性是否为#icon-setting
+{
+ const Constructor = Vue.extend(Button)
+ const button = new Constructor({
+     propsData:{
+         icon:'setting',
+     }
+ })
+ button.$mount()
+ let useElement = button.$el.querySelector('use')
+ let href = useElement.getAttribute('xlink:href')
+ expect(href).to.eq('#icon-setting')
+}
+//判断button的属性是否为#icon-loading
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData:{
+            icon:'setting',
+            loading:true
+        }
+    })
+    button.$mount()
+    let useElement = button.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-loading')
+}
+//判断button的style属性中的order属性为几
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData:{
+            icon:'setting',
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    //测试完后
+    //button元素删除
+    button.$el.remove()
+    //button对象删除
+    button.$destroy()
+}
+//判断添加了right的button的order是否为2
+{
+    const div2 = document.createElement('div')
+    document.body.appendChild(div2)
+
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData:{
+            icon:'setting',
+            iconPosition:"right"
+        }
+    })
+    button.$mount(div2)
+    let svg = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    //测试完后
+    //button元素删除
+    button.$el.remove()
+    //button对象删除
+    button.$destroy()
+}
+//监听click
+
